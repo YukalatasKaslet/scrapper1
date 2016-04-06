c6c5cbf1bfc20e1793f937d9148ef6d82461629e
codeamac09 <codeamac09@gmail.com>
2016-04-06 14:19:50 -0500

    Initialize Class Twitter_scrapper

---
+++ twitter_scrapper.rb
@@ -0,0 +1,14 @@
+class TwitterScrapper
+    def initalize(url)
+    end
+
+    def extract_username
+    end
+
+    def extract_tweets
+    end
+
+    def estract_stats
+    end
+
+end#class TwitterScrapper
